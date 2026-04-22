ac7706301af8320f20bb18ee7d4d96add5b4159a
Akhil Barapati <akhil.barapati@myemail.indwes.edu>
2026-04-22 17:07:16 -0400

    Day 8: implemented addEventListener and event handling

--- webbasics-1/DAY1-HTML/script.js
+++ webbasics-1/DAY1-HTML/script.js
@@ -1,3 +1,22 @@
+// Step 1: Get elements from HTML
+let alertButton = document.getElementById("alertBtn");
+let changeButton = document.getElementById("changeBtn");
+let resetButton = document.getElementById("resetBtn");
+
+// Step 2: Attach events
+alertButton.addEventListener("click", showAlert);
+changeButton.addEventListener("click", changeText);
+resetButton.addEventListener("click", resetText);
+
+// Step 3: Define functions
+function showAlert() {
+    alert("Akhil Hello from Day 8!");
+}
+
 function changeText() {
-    document.getElementById("message").innerText="Welcome Akhil Barapat!";
+    document.getElementById("text").innerText = "Text Changed!";
+}
+
+function resetText() {
+    document.getElementById("text").innerText = "Original Text";
 }
