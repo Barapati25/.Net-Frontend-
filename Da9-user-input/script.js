99b2bc6c994f9167f57790b770deee1f2289bca0
Akhil Barapati <akhil.barapati@myemail.indwes.edu>
2026-04-23 13:38:00 -0400

    Day9-Date and Time

--- Da9-user-input/script.js
+++ Da9-user-input/script.js
@@ -3,11 +3,23 @@ let resetbtn = document.getElementById("resetbtn");
 buttton.addEventListener("click", showName);
 resetbtn.addEventListener("click",resetData);
 
-function showname (
-) {
-    let name = document.getElementById("nameinput").value;
-    document.getElementById("output").innerText="Hello"+name;
+function showName() {
+    let name = document.getElementById("nameInput").value.trim();
 
+    if (name === "") {
+        alert("Please enter your name");
+    } else {
+        let hour = new Date().getHours();
+        let greeting;
+        if (hour < 12){
+            greeting = "Good Morning";
+
+        }else if (hour<18){
+            greeting = "Good Afternoon";
+        }else {greeting="Good Evening";}
+        let upperName = name.toUpperCase();
+        document.getElementById("output").innerText = `${greeting} ${name}`;
+    }
 }
 function resetData(){
  document.getElementById("nameinput").value ="";
